7ea6de6a7de02c20d5c34ca8c2928f27cec33b1c
Elkin Almonacid <elkinyamith13@gmail.com>
2024-08-20 13:42:28 -0500

    Primer commit

---
+++ src/Main.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int num=0;
+        int mon, total20, total50, total100, total200, total500, total;
+        int cont20=0, cont50=0, cont100=0, cont200=0, cont500=0;
+
+        while(num<5){
+            System.out.println("1. AGREGAR UNA MONEDA");
+            System.out.println("2. CONTAR MONEDAS");
+            System.out.println("3. CALCULAR EL TOTAL DEL DINERO");
+            System.out.println("4. ROMPER LA ALCANCIA");
+            System.out.println("ELIJA UNA OPCION:");
+            num=teclado.nextInt();
+
+            switch (num){
+                case 1:
+                    System.out.println("Moneda a añadir:");
+                    System.out.println("1. $20");
+                    System.out.println("2. $50");
+                    System.out.println("3. $100");
+                    System.out.println("4. $200");
+                    System.out.println("5. $500");
+                    mon=teclado.nextInt();
+                    if(mon==1){
+                        cont20++;
+                    }if(mon==2){
+                    cont50++;
+                    }if(mon==3){
+                    cont100++;
+                    }if(mon==4){
+                    cont200++;
+                    }if (mon==5){
+                    cont500++;
+                    }
+                    break;
+                case 2:
+                    System.out.println("Monedas de $20: "+cont20);
+                    System.out.println("Monedas de $50: "+cont50);
+                    System.out.println("Monedas de $100: "+cont100);
+                    System.out.println("Monedas de $200: "+cont200);
+                    System.out.println("Monedas de $500: "+cont500);
+                    break;
+                case 3:
+                    total20=cont20*20;
+                    total50=cont50*50;
+                    total100=cont100*100;
+                    total200=cont200*200;
+                    total500=cont500*500;
+                    total=total20+total50+total100+total200+total500;
+                    System.out.println("El total de dinero ahorrado es $"+total);
+                    break;
+                case 4:
+                    cont20=0;
+                    cont50=0;
+                    cont100=0;
+                    cont200=0;
+                    cont500=0;
+                    total20=0;
+                    total50=0;
+                    total100=0;
+                    total200=0;
+                    total500=0;
+                    break;
+                default:
+                    System.out.println("LA OPCION NO EXISTE");
+            }
+        }
+    }
+}
